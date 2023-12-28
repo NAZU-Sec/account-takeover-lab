05f1172f1dca1a2d012788d5c0b369a8de576432
mrnazu <samuelamsalu1998@gmail.com>
2023-12-28 17:21:17 +0300

    Routes: login, register, hompepage & levels

--- app/app.js
+++ app/app.js
@@ -2,25 +2,50 @@
 
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
+
+
+// Route
+// register page
+app.post('/register', function(req, res){
+	try{
+		res.status(201);
+		res.send("User Register!")
+	} catch (err){
+		res.status(400);
+		res.send(err.message)
+	}
+});
+
+// login page
+app.post('/login', function(req, res){
+	try{
+		res.status(201);
+		res.send("User Login!")
+	} catch (err){
+		res.status(400);
+		res.send(err.message)
+	}
+});
+
+// Home page
 app.get('/', function(req, res){
+	res.status(200);
 	res.send("Hello Hackers!")
 });
 
-app.get('/level1', function(req, res){
-	res.send('Easy level Lab!')
+app.use('/level', function(req, res){
+	if (req.method === 'GET') {
+    res.send('Easy level Lab!');
+  } else if (req.method === 'POST') {
+    console.log('Received POST data:', req.body);
+    res.send('Solution submitted successfully!');
+  } else {
+    res.status(405).send('Method not allowed');
+  }
 });
 
-app.get('/level2', function(req, res){
-	res.send('Medium level Lab!')
-});
-
-app.get('/level3', function(req, res){
-	res.send('Hard level Lab!')
-});
-
-app.get('/level4', function(req, res){
-	res.send('Very Hard level Lab!')
-});
 
 module.exports = app;
