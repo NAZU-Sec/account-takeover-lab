3afb25f40b2ca592df6dc8fac263fb03d8be25e1
mrnazu <samuelamsalu1998@gmail.com>
2023-12-28 22:53:06 +0300

    main login page controller done

---
+++ controllers/loginController.js
@@ -0,0 +1,49 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+
+function validateInputs(req) {
+  const errors = [];
+
+  // Validate username
+  const username = req.body.username;
+  if (!username || username.length < 5 || !/^[a-zA-Z0-9_]+$/.test(username)) {
+    errors.push("Username is required and must be at least 5 characters long. It can only contain letters, numbers, and underscores.");
+  }
+
+  // Validate password
+  const password = req.body.password;
+  if (!password || password.length < 8) {
+    errors.push("Password is required and must be at least 8 characters long.");
+  }
+
+  return errors.length === 0 ? null : errors;
+}
+
+module.exports.loginUser = async (req, res) => {
+  try {
+    // Validate inputs
+    const validationErrors = validateInputs(req);
+    if (validationErrors) {
+      res.status(400).json({ errors: validationErrors });
+      return;
+    }
+
+    const { username, password } = req.body;
+
+    // Find user in database
+    const user = await User.findOne({ username });
+
+    // Check if user exists and password matches
+    if (user && await bcrypt.compare(password, user.password)) {
+      // Create session (if using sessions)
+      req.session.user = user; // Ensure session middleware is configured
+
+      res.status(200).json({ message: "Login successful!" });
+    } else {
+      res.status(401).json({ error: "Invalid username or password" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Login failed due to an unexpected error" });
+  }
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const hunterRouter = express.Router();
 const registerUser = require('../controllers/registerController').registerUser;
+const loginUser = require('../controllers/loginController').loginUser;
 
 // Hunter register page
 hunterRouter.post('/register', registerUser);
 
 // Hunter login page
-hunterRouter.post('/login', function(req, res){
-	try{
-		res.status(201);
-		res.send("User Login!")
-	} catch (err){
-		res.status(400);
-		res.send(err.message)
-	}
-})
+hunterRouter.post('/login', loginUser)
 
 // Hunter home page
 hunterRouter.get('/', function(req, res){
