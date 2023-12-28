19c3ee38992f0b6dc95290076da99bf602e47298
mrnazu <samuelamsalu1998@gmail.com>
2023-12-28 14:44:21 +0300

    basic route app.js

--- app/app.js
+++ app/app.js
@@ -3,4 +3,24 @@
 const express = require('express');
 const app = express();
 
-module.exprets = app;
+app.get('/', function(req, res){
+	res.send("Hello Hackers!")
+});
+
+app.get('/level1', function(req, res){
+	res.send('Easy level Lab!')
+});
+
+app.get('/level2', function(req, res){
+	res.send('Medium level Lab!')
+});
+
+app.get('/level3', function(req, res){
+	res.send('Hard level Lab!')
+});
+
+app.get('/level4', function(req, res){
+	res.send('Very Hard level Lab!')
+});
+
+module.exports = app;
