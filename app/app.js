30963d319e3444a0fb4f4c03d5107b6f8c77436f
mrnazu <samuelamsalu1998@gmail.com>
2023-12-28 21:31:41 +0300

    session and cookie for the main app

--- app/app.js
+++ app/app.js
@@ -7,6 +7,17 @@ app.use(morgan('dev'));
 const hunterRouter = require('../routes/userRoute')
 const chalRouter = require('../routes/chalRoute')
 
+// sesssion
+const expressSession = require('express-session');
+const cookieParser = require('cookie-parser');
+
+app.use(expressSession({
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false,
+}));
+
+app.use(cookieParser());
 
 // Main Route
 app.use('/', hunterRouter);
