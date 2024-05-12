47258e15b0e1ea4db1d43e865fc8c45a517165b2
mrnazu <samuelamsalu1998@gmail.com>
2024-05-12 04:14:37 -0700

    #Chal1 Started implementing the brute force attacks challenge for level 1
    - Created a new route for level 1 task 1
    - Implemented GET request handler for level 1 task 1
    - Created a new EJS view file for the challenge
    - Passed a welcome message to the view file
    - Implemented a basic login form in the view file
    - Implemented POST request handler for level 1 task 1
    - Checked if the submitted username and password match the correct values
    - Redirected the user to the congratulations page if the login is successful
    - Redirected the user back to the login page if the login is unsuccessful
    - Created a new route for the congratulations page
    - Implemented GET request handler for the congratulations page
    - Displayed a success message on the congratulations page
    
    TODO:
    - Add more levels to the challenges
    - Implement more complex attack scenarios
    - Improve error handling and validation
    - Add user authentication and authorization
    - Add more security measures to prevent attacks
    
    Note: We have started implementing the brute force attacks challenge for level 1, but we have paused at this point. We will continue from this place in the future and add more features and functionalities to the challenge.

--- routes/challenges.js
+++ routes/challenges.js
@@ -1,10 +1,15 @@
-const express = require('express');
+// routes/challenges.js
 
+const express = require('express');
 const router = express.Router();
 
+
+// level 1
 router.get('/level1', (req, res) => {
+  console.log('Accessed level1 route');
   res.send('Welcome to level 1 of the challenges!');
 });
+
 
 router.get('/level2', (req, res) => {
   res.send('Welcome to level 2 of the challenges!');
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -8,7 +7,34 @@ router.get('/', (req, res) => {
 
 router.get('/level1', (req, res) => {
   res.send('This is level 1 of the dashboard.');
+  console.log('Accessed level1 route');
 });
+
+// GET level1: brute-force attacks
+router.get('/level1/task-1', (req, res) => {
+  console.log('Accessed level1/task-1 route');
+  res.render('../challenges/level1/brute-force-attacks/index.ejs');
+});
+// POST level1: brute-force attacks
+router.post('/level1/task-1', (req, res) => {
+  console.log('Accessed level1/task-1 POST route');
+  const { username, password } = req.body;
+
+  // Check if the username and password are correct
+  if (username === 'account_takeover_user' && password === 'account_takeover_pass') {
+    // Redirect the user to the congratulations page
+    res.redirect('/level1/task-1/congratulations');
+  } else {
+    // Redirect the user back to the login page
+    res.redirect('/level1/task-1');
+  }
+});
+
+router.get('/level1/task-1/congratulations', (req, res) => {
+  console.log('Accessed level1/task-1/congratulations route');
+  res.send('Congratulations! You have successfully logged in.');
+});
+
 
 router.get('/level2', (req, res) => {
   res.send('This is level 2 of the dashboard.');
